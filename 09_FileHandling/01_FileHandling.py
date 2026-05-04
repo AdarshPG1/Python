f487bcbe734ab7d6ed9841c32d999d39fda65ce5
Adarsh Gayadolli <gayadolliadarsh@gmail.com>
2026-05-04 10:44:30 +0000

    file handling

---
+++ 09_FileHandling/01_FileHandling.py
@@ -0,0 +1,11 @@
+# f=open("Adarsh.txt","x")
+
+# f=open("Adarsh.txt","r")
+# print(f.read())
+
+# f=open("Adarsh.txt","a")
+# f.write("This is Additional info")
+
+# f=open("Adarsh.txt","r")
+# print(f.read())
+# f.close()
---
+++ 09_FileHandling/02_FH.py
@@ -0,0 +1,13 @@
+# f=open("file.txt","x")
+# print(f.read())
+
+# f=open("file.txt","a")
+# f.write("Adarsh")
+# f=open("file.txt","r")
+# print(f.read())
+
+# f=open("D:\est.txt","r")
+# print(f.read())
+
+with open("Adarsh.txt","r")as f:
+    print(f.read())
